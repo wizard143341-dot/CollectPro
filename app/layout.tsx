c861103638b21e61745cd04d927ecd614747741f
wizard143341-dot <wizard143.341@gmail.com>
2026-05-09 19:49:28 +0530

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
